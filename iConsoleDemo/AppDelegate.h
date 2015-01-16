0e50cb866ecd0a4911bf491f64034f85c2c5e468
Wildyao <z8927623@163.com>
2015-01-16 16:55:47 +0800

    Initial Commit

---
+++ iConsoleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iConsoleDemo
+//
+//  Created by wildyao on 15/1/16.
+//  Copyright (c) 2015年 Wild Yaoyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iConsoleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iConsoleDemo
+//
+//  Created by wildyao on 15/1/16.
+//  Copyright (c) 2015年 Wild Yaoyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
